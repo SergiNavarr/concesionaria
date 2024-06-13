dc72e9b189c473d642fe21b3acae351b9bb846b3
Sergio Navarro <nachonavarro197@gmail.com>
2024-06-13 07:27:13 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/concesionaria/Concesionaria.java
@@ -0,0 +1,12 @@
+package com.mycompany.concesionaria;
+
+import com.mycompany.concesionaria.igu.Principal;
+
+public class Concesionaria {
+
+    public static void main(String[] args) {
+        Principal princ = new Principal();
+        princ.setVisible(true);
+        princ.setLocationRelativeTo(null);
+    }
+}
